20983f0725aa300b97708965cf0c343bfbd94b34
rohit-rt <rohit.rtsaini@gmail.com>
2025-10-11 21:38:10 +0530

    chg

--- static/js/main.js
+++ static/js/main.js
@@ -1,193 +1,404 @@
+(function($){
+	"use strict";
+
 /*
-Special Thanks - noirsociety
-Codepen URL - https://codepen.io/noirsociety/pen/BaGNrdz
+-----------------------------------------------------
+	Header Sticky
+-----------------------------------------------------
 */
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop() > 120){  
+			$('.navbar-area').addClass("sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("sticky");
+		}
+	});
+/*
+-----------------------------------------------------
+	Hover Button Effect
+-----------------------------------------------------
+*/
+	let toolTip = document.getElementById('tooltip');
+	if (toolTip) {
+		window.addEventListener('mousemove', toolTipXY);
+	}
+	function toolTipXY(e) {
+		let x = e.clientX,
+			y = e.clientY;
 
-const magnet = document.querySelector(".magnet");
-const cursor = document.querySelector(".magnet-cursor");
-const circle = document.querySelector(".magnet-circle");
-let [startX, startY, endX, endY] = [0, 0, 0, 0];
-let time = 0.1;
-let raf;
+		if (toolTip) {
+			toolTip.style.top = (y + 0) + 'px';
+			toolTip.style.left = (x + 0) + 'px';
+		}
+	}
+/*
+-----------------------------------------------------
+	 Team Slides
+-----------------------------------------------------
+*/
+	 $('.team-card').owlCarousel({
+		nav: true,
+		loop: true,
+		dots: false,
+		margin: 10,
+		autoplay: true,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='fi fi-tr-arrow-small-left' ></i>",
+			"<i class='fi fi-tr-arrow-small-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 1
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 2
+			}
+		}
+	});
+/*
+-----------------------------------------------------
+	 developes Slides
+-----------------------------------------------------
+*/
+	 $('.developes-card').owlCarousel({
+		nav: false,
+		loop: true,
+		dots: false,
+		margin: 10,
+		autoplay: true,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='fi fi-tr-arrow-small-left' ></i>",
+			"<i class='fi fi-tr-arrow-small-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 3
+			}
+		}
+	});
+/*
+-----------------------------------------------------
+	 Testimonial Slides
+-----------------------------------------------------
+*/
+	 $('.testimonial-content').owlCarousel({
+		nav: true,
+		loop: true,
+		dots: false,
+		margin: 20,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed:1000,
+		navText: [
+			"<i class='fi fi-tr-arrow-left'></i>",
+			"<i class='fi fi-tr-arrow-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 2
+			},
+			1200: {
+				items: 3
 
-const lerp = (start, end, t) => start * (1 - t) + end * t;
+			}
+		}
+	});
+/*
+-----------------------------------------------------
+	Workflow Slides
+-----------------------------------------------------
+*/
+	 $('.workflow-wrapper').owlCarousel({
+		nav: false,
+		loop: true,
+		dots: false,
+		margin: 20,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed:1000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 2
+			},
+			1200: {
+				items: 2
 
-function updateCircle() {
-  startX = lerp(startX, endX, time);
-  startY = lerp(startY, endY, time);
-  circle.style.transform = `translate(${startX}px,${startY}px)`;
-  raf = requestAnimationFrame(updateCircle);
-  startX.toFixed(1) === endX.toFixed(1) && cancelAnimationFrame(raf);
-}
+			}
+		}
+	});
+/*
+-----------------------------------------------------
+	 Testimonial Slides 2
+-----------------------------------------------------
+*/
+	 $('.testimonial-content-2').owlCarousel({
+		nav: false,
+		loop: true,
+		dots: true,
+		margin: 20,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed:1000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 1
+			},
+			992: {
+				items: 1
+			},
+			1200: {
+				items: 2
 
-function trackCircle(e) {
-  endX = e.clientX - circle.clientWidth / 2;
-  endY = e.clientY - circle.clientHeight / 2;
-  cancelAnimationFrame(raf);
-  raf = requestAnimationFrame(updateCircle);
-}
+			}
+		}
+	});
+/*
+-----------------------------------------------------
+	Popup Image
+-----------------------------------------------------
+*/
+	$('.popup-btn').magnificPopup({
+		type: 'image',
+		gallery:{
+			enabled:true
+		}
+	});
 
-function trackCursor(e) {
-  cursor.style.setProperty("--x", `${e.clientX - cursor.clientWidth / 2}px`);
-  cursor.style.setProperty("--y", `${e.clientY - cursor.clientHeight / 2}px`);
-  magnet.classList.remove("reset");
-}
+/*
+-----------------------------------------------------
+	Counter Js
+-----------------------------------------------------
+*/
+	if ("IntersectionObserver" in window) {
+	let counterObserver = new IntersectionObserver(function (entries, observer) {
+		entries.forEach(function (entry) {
+			if (entry.isIntersecting) {
+			let counter = entry.target;
+			let target = parseInt(counter.innerText);
+			let step = target / 200;
+			let current = 0;
+			let timer = setInterval(function () {
+				current += step;
+				counter.innerText = Math.floor(current);
+				if (parseInt(counter.innerText) >= target) {
+				clearInterval(timer);
+				}
+			}, 10);
+			counterObserver.unobserve(counter);
+			}
+		});
+	});
 
-function init(e) {
-  trackCursor(e);
-  trackCircle(e);
-  return e.target.matches(".magnet-link") && magnet.classList.add("reset");
-}
-
-window.addEventListener("pointermove", init, false);
-window.addEventListener("load", updateCircle, false);
-
-
-
-
-
-
-!(function(a) {
-    "use strict";
-    var e;
-    a(window).stellar({ responsive: !0, parallaxBackgrounds: !0, parallaxElements: !0, horizontalScrolling: !1, hideDistantElements: !1, scrollProperty: "scroll" }),
-        a(".js-fullheight").css("height", a(window).height()),
-        a(window).resize(function() {
-            a(".js-fullheight").css("height", a(window).height());
-        }),
-        setTimeout(function() {
-            a("#ftco-loader").length > 0 && a("#ftco-loader").removeClass("show");
-        }, 1),
-        a.Scrollax(),
-        a("body").on("click", ".js-fh5co-nav-toggle", function(e) {
-            e.preventDefault(), a("#ftco-nav").is(":visible") ? a(this).removeClass("active") : a(this).addClass("active");
-        }),
-        a(document).on("click", '#ftco-nav a[href^="#"]', function(e) {
-            e.preventDefault(), a.attr(this, "href"), a("html, body").animate({ scrollTop: a(a.attr(this, "href")).offset().top - 70 }, 500, function() {});
-        }),
-        a(".home-slider").owlCarousel({
-            loop: !0,
-            autoplay: !1,
-            margin: 0,
-            animateOut: "fadeOut",
-            animateIn: "fadeIn",
-            nav: !1,
-            autoplayHoverPause: !0,
-            items: 1,
-            navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
-            responsive: { 0: { items: 1 }, 600: { items: 1 }, 1e3: { items: 1 } },
-        }),
-        a(".carousel-testimony").owlCarousel({
-            center: !0,
-            loop: !0,
-            autoplay: !0,
-            autoplaySpeed: 2e3,
-            items: 1,
-            margin: 30,
-            stagePadding: 0,
-            nav: !1,
-            navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-            responsive: { 0: { items: 1 }, 600: { items: 2 }, 1e3: { items: 3 } },
-        }),
-        a(".carousel-projects").owlCarousel({
-            center: !0,
-            loop: !0,
-            autoplay: !0,
-            autoplaySpeed: 1e3,
-            items: 1,
-            pauseOnHover: !0,
-            margin: 30,
-            stagePadding: 0,
-            dots: false,
-            nav: true,
-            navText: ["<", ">"],
-            responsive: { 0: { items: 1 }, 600: { items: 2 }, 1e3: { items: 3 } },
-        }),
-        a("nav .dropdown").hover(
-            function() {
-                var e = a(this);
-                e.addClass("show"), e.find("> a").attr("aria-expanded", !0), e.find(".dropdown-menu").addClass("show");
-            },
-            function() {
-                var e = a(this);
-                e.removeClass("show"), e.find("> a").attr("aria-expanded", !1), e.find(".dropdown-menu").removeClass("show");
-            }
-        ),
-        a("#dropdown04").on("show.bs.dropdown", function() {
-            console.log("show");
-        }),
-        a(window).scroll(function() {
-            var e = a(this).scrollTop(),
-                s = a(".ftco_navbar"),
-                t = a(".js-scroll-wrap");
-            e > 150 && !s.hasClass("scrolled") && s.addClass("scrolled"),
-                e < 150 && s.hasClass("scrolled") && s.removeClass("scrolled sleep"),
-                e > 350 && (s.hasClass("awake") || s.addClass("awake"), t.length > 0 && t.addClass("sleep")),
-                e < 350 && (s.hasClass("awake") && (s.removeClass("awake"), s.addClass("sleep")), t.length > 0 && t.removeClass("sleep"));
-        }),
-        a("#section-counter, .hero-wrap, .ftco-counter").waypoint(
-            function(e) {
-                if ("down" === e && !a(this.element).hasClass("ftco-animated")) {
-                    var s = a.animateNumber.numberStepFactories.separator(",");
-                    a(".number").each(function() {
-                        var e = a(this),
-                            t = e.data("number");
-                        console.log(t), e.animateNumber({ number: t, numberStep: s }, 7e3);
-                    });
-                }
-            }, { offset: "95%" }
-        ),
-        (e = 0),
-        a(".ftco-animate").waypoint(
-            function(s) {
-                "down" !== s ||
-                    a(this.element).hasClass("ftco-animated") ||
-                    (e++,
-                        a(this.element).addClass("item-animate"),
-                        setTimeout(function() {
-                            a("body .ftco-animate.item-animate").each(function(e) {
-                                var s = a(this);
-                                setTimeout(
-                                    function() {
-                                        var a = s.data("animate-effect");
-                                        "fadeIn" === a
-                                            ?
-                                            s.addClass("fadeIn ftco-animated") :
-                                            "fadeInLeft" === a ?
-                                            s.addClass("fadeInLeft ftco-animated") :
-                                            "fadeInRight" === a ?
-                                            s.addClass("fadeInRight ftco-animated") :
-                                            s.addClass("fadeInUp ftco-animated"),
-                                            s.removeClass("item-animate");
-                                    },
-                                    50 * e,
-                                    "easeInOutExpo"
-                                );
-                            });
-                        }, 100));
-            }, { offset: "95%" }
-        ),
-        a(".image-popup").magnificPopup({
-            type: "image",
-            closeOnContentClick: !0,
-            closeBtnInside: !1,
-            fixedContentPos: !0,
-            mainClass: "mfp-no-margins mfp-with-zoom",
-            gallery: { enabled: !0, navigateByImgClick: !0, preload: [0, 1] },
-            image: { verticalFit: !0 },
-            zoom: { enabled: !0, duration: 300 },
-        }),
-        a(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({ disableOn: 700, type: "iframe", mainClass: "mfp-fade", removalDelay: 160, preloader: !1, fixedContentPos: !1 });
-})(jQuery),
-$(function() {
-    $(".progress").each(function() {
-        var e = $(this).attr("data-value"),
-            s = $(this).find(".progress-left .progress-bar"),
-            t = $(this).find(".progress-right .progress-bar");
-        e > 0 && (e <= 50 ? t.css("transform", "rotate(" + a(e) + "deg)") : (t.css("transform", "rotate(180deg)"), s.css("transform", "rotate(" + a(e - 50) + "deg)")));
-    });
-
-    function a(a) {
-        return (a / 100) * 360;
+	let counters = document.querySelectorAll(".counter-num");
+	counters.forEach(function (counter) {
+		counterObserver.observe(counter);
+	});
     }
-});
+
+/*
+-----------------------------------------------------
+	Gallery MixItUp 
+-----------------------------------------------------
+*/
+	$(function () {
+		var filterList = {
+			init: function () {
+				$('.item-grid').mixItUp({
+					selectors: {
+					target: '.item',
+					filter: '.filter'	
+				},
+				load: {
+				  filter: 'all'
+				}     
+				});								
+			}
+		};
+		filterList.init();
+	});	
+
+/*
+-----------------------------------------------------
+	Popup Video
+-----------------------------------------------------
+*/
+	$('.popup-youtube').magnificPopup({
+		disableOn: 320,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+/*
+-----------------------------------------------------
+	Input Plus & Minus Number JS
+-----------------------------------------------------
+*/
+	$('.input-counter').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="text"]'),
+		btnUp = spinner.find('.plus-btn'),
+		btnDown = spinner.find('.minus-btn'),
+		min = input.attr('min'),
+		max = input.attr('max');
+		
+		btnUp.on('click', function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+		btnDown.on('click', function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+	});
+/*
+-----------------------------------------------------
+	Meetai Testimonial
+-----------------------------------------------------
+*/
+	
+	$('.aithm-testimonial-navigator').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		items: 1,
+		navText: [
+			"<i class='bx bx-left-arrow-alt'></i>",
+			"<i class='bx bx-right-arrow-alt'></i>"
+		]
+	});
+
+/*
+-----------------------------------------------------
+	Faq Section
+-----------------------------------------------------
+*/
+	const faqItems = document.querySelectorAll('.faq-item');
+
+	faqItems.forEach(item => {
+	const question = item.querySelector('.faq-question');
+	const answer = item.nextElementSibling;
+	const icon = item.querySelector('i');
+	
+	item.addEventListener('click', () => {
+		faqItems.forEach(otherItem => {
+		if (otherItem !== item) {
+			const otherAnswer = otherItem.nextElementSibling;
+			const otherIcon = otherItem.querySelector('i');
+	
+			otherAnswer.classList.remove('active');
+			otherIcon.classList.remove('active');
+			otherAnswer.style.maxHeight = "0";
+		}
+		});
+	
+		answer.classList.toggle('active');
+		icon.classList.toggle('active');
+		if (answer.classList.contains('active')) {
+		answer.style.maxHeight = answer.scrollHeight + "px";
+		} else {
+		answer.style.maxHeight = "0";
+		}
+	});
+	});
+
+/*
+-----------------------------------------------------
+	Aos Animation
+-----------------------------------------------------
+*/
+	AOS.init({
+		once: true,
+		offset: 200,
+		duration: 2000,
+	});
+
+/*
+-----------------------------------------------------
+	Go to Top
+-----------------------------------------------------
+*/
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+
+/*
+-----------------------------------------------------
+	Nice Select
+-----------------------------------------------------
+*/
+	$('select').niceSelect();
+
+}(jQuery));
